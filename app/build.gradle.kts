d6ac1b90cc0aacef76fe03c1f58ab08b53b2d85f
Kevin Qian <kevinqian28@hotmail.com>
2023-10-30 15:39:28 -0600

    Updated SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.finding_tory"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.finding_tory"
