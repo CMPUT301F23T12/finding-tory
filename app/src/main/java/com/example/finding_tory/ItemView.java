6941292ba3497810d8494cc31b1f044dbad05871
Aakash1417 <durairaj@ualberta.ca>
2023-11-08 15:58:45 -0700

    delete fragment done, fixed function names in ItemView and refactored it

--- app/src/main/java/com/example/finding_tory/ItemView.java
+++ app/src/main/java/com/example/finding_tory/ItemView.java
@@ -1,15 +1,21 @@
 package com.example.finding_tory;
 
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ItemActivity extends AppCompatActivity {
+public class ItemView extends AppCompatActivity {
+    private ActivityResultLauncher<Intent> editItemActivityResultLauncher;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.item_click_view);
@@ -18,20 +24,18 @@ public class ItemActivity extends AppCompatActivity {
         Item selectedItem = (Item) getIntent().getSerializableExtra("item");
 
         // Use the selectedItem to populate the views in the item_click_view layout
-        TextView item_name = findViewById(R.id.item_name_text);
-        item_name.setText(selectedItem.getName());
-        TextView item_comment = findViewById(R.id.item_comment_text);
-        item_comment.setText(selectedItem.getComment());
-        TextView item_make = findViewById(R.id.item_make_text);
-        item_make.setText(selectedItem.getMake());
-        TextView item_date = findViewById(R.id.item_date_text);
-        item_date.setText(selectedItem.getDate().toString());
-        TextView item_value = findViewById(R.id.item_value_text);
-        item_value.setText(selectedItem.getValue().toString());
-        TextView item_quantity = findViewById(R.id.item_quantity_text);
-        item_quantity.setText(selectedItem.getMake().toString());
-        TextView item_serial_number = findViewById(R.id.item_serial_number_text);
-        item_serial_number.setText(selectedItem.getSerialNumber());
+        setItemView(selectedItem);
+
+        // Register the activity result launcher
+        editItemActivityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+                // Handle the returned result data
+                Intent data = result.getData();
+                selectedItem = (Item) data.getSerializableExtra("editedItem");
+                // Update your UI with the edited item here
+                setItemView(selectedItem);
+            }
+        });
 
 
         // Handles going back to Inventory view
@@ -46,27 +50,27 @@ public class ItemActivity extends AppCompatActivity {
         final Button deleteItemButton = findViewById(R.id.button_delete_item);
         deleteItemButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 // Show Delete Confirmation Fragment
-                DeleteItemFragment().show(getSupportFragmentManager(), "DELETE_ITEM");
+                DeleteItemFragment deleteDialog = new DeleteItemFragment();
+                deleteDialog.show(getSupportFragmentManager(), "DELETE_ITEM");
             }
         });
-
         final Button editItemButton = findViewById(R.id.button_edit_item);
         editItemButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 // Start Edit Activity
-                Intent editItemIntent = new Intent(ItemActivity.this, EditItemActivity.class);
+                Intent editItemIntent = new Intent(ItemView.this, EditItemActivity.class);
                 editItemIntent.putExtra("selectedItem", selectedItem);
-                startActivity(editItemIntent);
+                editItemActivityResultLauncher.launch(editItemIntent);
             }
         });
 
         final Button leftPictureButton = findViewById(R.id.button_left_picture_item);
         leftPictureButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 // Will Implement Later
             }
         });
@@ -74,9 +78,26 @@ public class ItemActivity extends AppCompatActivity {
         final Button rightPictureButton = findViewById(R.id.button_right_picture_item);
         rightPictureButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 // Will Implement Later
             }
         });
+    }
+
+    private void setItemView(Item passedItem) {
+        TextView item_name = findViewById(R.id.item_name_text);
+        // item_name.setText(passedItem.getName());
+        TextView item_comment = findViewById(R.id.item_comment_text);
+        // item_comment.setText(passedItem.getComment());
+        TextView item_make = findViewById(R.id.item_make_text);
+        // item_make.setText(passedItem.getMake());
+        TextView item_date = findViewById(R.id.item_date_text);
+        // item_date.setText(passedItem.getDate().toString());
+        TextView item_value = findViewById(R.id.item_value_text);
+        // item_value.setText(passedItem.getValue().toString());
+        TextView item_quantity = findViewById(R.id.item_quantity_text);
+        // item_quantity.setText(passedItem.getMake().toString());
+        TextView item_serial_number = findViewById(R.id.item_serial_number_text);
+        // item_serial_number.setText(passedItem.getSerialNumber());
     }
 }
