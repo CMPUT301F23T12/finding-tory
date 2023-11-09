f94635c994525582b5d6c8bb541755265f4033bc
parshva-s <parshva.s24@gmail.com>
2023-11-09 10:33:41 -0700

    Doc strings added to Item

--- app/src/main/java/com/example/finding_tory/Item.java
+++ app/src/main/java/com/example/finding_tory/Item.java
@@ -6,17 +6,19 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
+/**
+ * Class to represent the attributes of Items
+ */
 public class Item implements Serializable {
-    Date purchaseDate;
-    String description;
-    String make;
-    String model;
-    String serialNumber;
-    float estimatedValue;
-    String comment;
-    ArrayList<String> itemTags;
+    private Date purchaseDate;
+    private String description;
+    private String make;
+    private String model;
+    private String serialNumber;
+    private float estimatedValue;
+    private String comment;
+    private ArrayList<String> itemTags;
     // TODO: Add ArrayList of Images
-
 
     public Item(Date purchaseDate, String description, String make, String model, float estimatedValue, String serialNumber, String comment, ArrayList<String> itemTags) {
         this.purchaseDate = purchaseDate;
@@ -29,6 +31,10 @@ public class Item implements Serializable {
         this.itemTags = itemTags;
     }
 
+    /**
+     * Copy the attributes of another item to this item
+     * @param copy the item to be copied
+     */
     public void updateItem(Item copy) {
         this.purchaseDate = copy.getPurchaseDate();
         this.description = copy.getDescription();
@@ -40,81 +46,158 @@ public class Item implements Serializable {
         this.itemTags = copy.getItemTags();
     }
 
+    /**
+     * Gets the purchase date of item
+     * @return purchase date
+     */
     public Date getPurchaseDate() {
         return purchaseDate;
     }
 
+    /**
+     * Sets the purchase date of the item
+     * @param purchaseDate new purchase date
+     */
     public void setPurchaseDate(Date purchaseDate) {
         this.purchaseDate = purchaseDate;
     }
 
+    /**
+     * Gets the description (name) of the item
+     * @return the item description
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Sets the description of the item
+     * @param description new description
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * Gets the make of the item
+     * @return the item make
+     */
     public String getMake() {
         return make;
     }
 
+    /**
+     * Sets the make of the item
+     * @param make new make
+     */
     public void setMake(String make) {
         this.make = make;
     }
 
+    /**
+     * Gets the model of the item
+     * @return the item model
+     */
     public String getModel() {
         return model;
     }
 
+    /**
+     * Sets the model of the item
+     * @param model new model
+     */
     public void setModel(String model) {
         this.model = model;
     }
 
+    /**
+     * Gets the serial number of the item
+     * @return the item serial number
+     */
     public String getSerialNumber() {
         return serialNumber;
     }
 
+    /**
+     * Sets the serial number of the item
+     * @param serialNumber new serial number
+     */
     public void setSerialNumber(String serialNumber) {
         this.serialNumber = serialNumber;
     }
 
+    /**
+     * Gets the estimated value of the item
+     * @return the estimated value
+     */
     public float getEstimatedValue() {
         return estimatedValue;
     }
 
+    /**
+     * Sets the estimated value of the item
+     * @param estimatedValue new estimated value
+     */
     public void setEstimatedValue(float estimatedValue) {
         this.estimatedValue = estimatedValue;
     }
 
+    /**
+     * Gets the the comment made about the item
+     * @return the comment
+     */
     public String getComment() {
         return comment;
     }
 
+    /**
+     * Sets the comment made for item
+     * @param comment new comment
+     */
     public void setComment(String comment) {
         this.comment = comment;
     }
 
+    /**
+     * Gets the tags attached to an item
+     * @return list of tags
+     */
     public ArrayList<String> getItemTags() {
         return itemTags;
     }
 
+    /**
+     * displays tags as a space separated list
+     * @return string to display tags
+     */
     public StringBuilder getTagsString() {
         StringBuilder tags_string = new StringBuilder();
         for (String tag: this.itemTags) {
             tags_string.append(tag).append(" ");
         }
-
         return tags_string;
     }
 
+    /**
+     * Sets the tags to a new list of tags
+     * @param itemTags new list of tags
+     */
     public void setItemTags(ArrayList<String> itemTags) {
         this.itemTags = itemTags;
     }
 
+    /**
+     * Checks inputs to ensure that the data being entered is valid
+     *
+     * @param purchaseDate the date that user inputs
+     * @param description the description users inputs
+     * @param estimatedValue the estimated value that user inputs
+     * @return a string with error message or empty string (no errors)
+     * @throws ParseException
+     */
     public static String errorHandleItemInput(String purchaseDate, String description, String estimatedValue) throws ParseException {
         if (description.trim().equals("")) return "Item Description cannot be empty";
+
         Date current = new Date();
         if (current.before(new SimpleDateFormat("yyyy-MM-dd").parse(purchaseDate)))
             return "Date cannot be in the future";
