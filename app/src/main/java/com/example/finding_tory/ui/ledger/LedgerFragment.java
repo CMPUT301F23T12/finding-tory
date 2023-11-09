1ebf101ef058e590a438206e432c5a11a68a3658
t-mannan <88060835+t-mannan@users.noreply.github.com>
2023-11-09 02:18:18 -0700

    Create LedgerFragment to display mock ledger, cleanup provided files
    no functionality yet, other than navbar

--- app/src/main/java/com/example/finding_tory/ui/ledger/LedgerFragment.java
+++ app/src/main/java/com/example/finding_tory/ui/ledger/LedgerFragment.java
@@ -1,42 +1,88 @@
 package com.example.finding_tory.ui.ledger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.fragment.app.FragmentActivity;
 
+import com.example.finding_tory.Inventory;
+import com.example.finding_tory.InventoryViewActivity;
+import com.example.finding_tory.LedgerAdapter;
 import com.example.finding_tory.databinding.FragmentLedgerBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.ArrayList;
+
+/**
+ * Displays the Ledger: a list of all the signed-in user's Inventories.
+ *  Serves as the homepage after signing in.
+ */
 public class LedgerFragment extends Fragment {
 
     private FragmentLedgerBinding binding;
 
+    private ArrayList<Inventory> inventories;
+    private ListView ledgerListView;
+    private LedgerAdapter ledgerAdapter;
+
+    private FloatingActionButton addInvButton;
+
+    /**
+     * Maps the ledger adapter to the underlying list of inventories, and sets
+     *  any click listeners for each list element/the add button.
+     *
+     * @param inflater The LayoutInflater object that can be used to inflate
+     * any views in the fragment,
+     * @param container If non-null, this is the parent view that the fragment's
+     * UI should be attached to.  The fragment should not add the view itself,
+     * but this can be used to generate the LayoutParams of the view.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     *
+     * @return the view that is created.
+     */
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        LedgerViewModel ledgerViewModel =
-                new ViewModelProvider(this).get(LedgerViewModel.class);
-
         binding = FragmentLedgerBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        // create some mock inventories to populate the list
+        inventories = new ArrayList<>();
+        inventories.add(new Inventory("Inventory 1"));
+
+        // map the listview to the ledger's list of items via custom ledger adapter
+        ledgerListView = binding.ledgerListview;
+        ledgerAdapter = new LedgerAdapter(root.getContext(), inventories);
+        ledgerListView.setAdapter(ledgerAdapter);
+
+        // cache the add button
+        addInvButton = binding.addInventoryButton;
+
+        // allow new inventories to be added
         binding.addInventoryButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Create an inventory (Coming soon!)", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
 
-        final TextView textView = binding.textLedger;
-        ledgerViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
+
+    /**
+     * Cleanup when this fragment's life cycle is over.
+     */
     @Override
     public void onDestroyView() {
         super.onDestroyView();
