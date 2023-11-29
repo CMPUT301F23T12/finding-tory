9bc528c835ab6ba103f37489127b678407e00fa3
parshva-s <parshva.s24@gmail.com>
2023-11-28 20:00:07 -0700

    Docstrings added to CaptureAct

--- app/src/main/java/com/example/finding_tory/CaptureAct.java
+++ app/src/main/java/com/example/finding_tory/CaptureAct.java
@@ -2,6 +2,9 @@ package com.example.finding_tory;
 
 import com.journeyapps.barcodescanner.CaptureActivity;
 
+/**
+ * Capture activity that allows barcode to be scanned
+ */
 public class CaptureAct extends CaptureActivity {
 
 }
